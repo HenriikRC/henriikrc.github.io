7b90feff70d1244b25193115f0314eb470a0246c
Henrik R. Christensen <112691706+HenriikRC@users.noreply.github.com>
2026-08-24 21:10:47 +0200

    Create config.js

---
+++ handball/config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDmv3DgIScVWP3tq2SSapS_R9w49gviCFI",
+    authDomain: "handball-26-27.firebaseapp.com",
+    projectId: "handball-26-27",
+    storageBucket: "handball-26-27.firebasestorage.app",
+    messagingSenderId: "660609138006",
+    appId: "1:660609138006:web:1b5871be1a46944c60aaf7",
+    measurementId: "G-BPJX8TMSMT"
+  };
